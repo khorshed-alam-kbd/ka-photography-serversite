197746850a22fdfe600d5c8b62c95df6668e3281
khorshed-alam-kbd <khorshedalam.kbd@gamil.com>
2022-11-10 19:40:00 +0600

    Ka Photography Server

--- index.js
+++ index.js
@@ -13,13 +13,25 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0zdruwc.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
 
     try {
-        const serviceCollection = client.db('kaPhotography').collection(services);
+        const serviceCollection = client.db('kaPhotography').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const quarry = {}
+            const cursor = serviceCollection.find(quarry)
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+        app.get('/services/3', async (req, res) => {
+            const quarry = {}
+            const cursor = serviceCollection.find(quarry).limit(3)
+            const services = await cursor.toArray();
+            res.send(services);
+        })
     }
     finally {
 
